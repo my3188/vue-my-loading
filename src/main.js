d19d537f1f89465847dc6fdcc83b016a35f0439c
Simon <my3188@gmail.com>
2020-10-25 13:50:00 +0800

    引入插件，同时在页面上使用

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import loading from "@/plugs/loading/index";
+import "@/plugs/loading/src/loading.css";
+Vue.use(loading);
 
 Vue.config.productionTip = false
 
